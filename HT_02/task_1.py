3e717d12fbe3ab2c4208752472bd405e2e0460c7
artem <danelenkoad91@gmail.com>
2023-10-18 22:10:00 +0300

    update task_1.py

--- HT_02/task_1.py
+++ HT_02/task_1.py
@@ -6,4 +6,5 @@ user and generate a list and a tuple with those numbers.
 numbers = input("Write a sequence of comma-separated numbers: ")
 numbers_list = [int(i) for i in numbers.replace(" ", "").split(",")]
 numbers_tuple = tuple(numbers_list)
+
 print(numbers_list, numbers_tuple, sep='\n')
