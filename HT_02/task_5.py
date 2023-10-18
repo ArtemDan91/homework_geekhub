0d7ec148f5f78cfae34dde3443c46e23a4384ff6
artem <danelenkoad91@gmail.com>
2023-10-18 22:10:41 +0300

    update task_5.py

--- HT_02/task_5.py
+++ HT_02/task_5.py
@@ -2,5 +2,6 @@
 
 decimal_number = int(input("Enter a decimal number: "))
 hexadecimal_number = hex(decimal_number)[2:].upper()
+
 print(f"Hexadecimal number of {decimal_number} is {hexadecimal_number}")
 
