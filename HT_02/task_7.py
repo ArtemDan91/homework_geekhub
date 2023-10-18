af69202869759fcc2795deae24064bb5009247bf
artem <danelenkoad91@gmail.com>
2023-10-18 22:11:06 +0300

    update task_7.py

--- HT_02/task_7.py
+++ HT_02/task_7.py
@@ -5,4 +5,5 @@ List must be include both strings and integers and must be hardcoded.
 
 my_list = ["Hello", 1, "world", 2, False]
 concatenate_string = ''.join([str(i) for i in my_list])
+
 print(concatenate_string)
