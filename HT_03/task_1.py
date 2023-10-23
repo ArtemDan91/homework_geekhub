c18435be319d103ba07a196770275c01a4f12171
artem <danelenkoad91@gmail.com>
2023-10-23 21:12:31 +0300

    update task_1.py

--- HT_03/task_1.py
+++ HT_03/task_1.py
@@ -4,19 +4,26 @@ for each tuple. The list of tuples can be hardcoded. The "replacement" value is
 entered by user. The number of elements in the tuples must be different.
 """
 
-list_of_tuples = [(1, 2, 3), (4, 5, 6, 7), (8, 9, 10, 11, 12)]
+list_of_tuples = [(), (1,), (2, 3), (4, 5, 6), (7, 8, 9, 10)]
 replacement_value = input("Enter the replacement value: ")
 
-print([i[:-1] + (replacement_value,) for i in list_of_tuples])
+for i in range(len(list_of_tuples)):
+    if list_of_tuples[i]:
+        list_of_tuples[i] = list_of_tuples[i][:-1] + (replacement_value,)
+
+print(list_of_tuples)
 
 
 # через обробку вводу користувача
-# list_of_tuples = [(1, 2, 3), (4, 5, 6, 7), (8, 9, 10, 11, 12)]
+# list_of_tuples = [(), (1,), (2, 3), (4, 5, 6), (7, 8, 9, 10)]
 # replacement_value = input("Please, enter an integer: ")
 
 # try:
 #     replacement_value = int(replacement_value)
-#     print([i[:-1] + (replacement_value,) for i in list_of_tuples])
+#     for i in range(len(list_of_tuples)):
+#         if list_of_tuples[i]:
+#             list_of_tuples[i] = list_of_tuples[i][:-1] + (replacement_value,)
+#     print(list_of_tuples)
 
 # except ValueError:
 #     print("Please, enter a valid replacement value.")
